50bc19681d96c6a6a0b54da59f7684f59bcbf289
dbeltran <daniel.beltran@irbbarcelona.org>
2026-06-03 11:11:18 +0200

    improved error logs in option counts

--- utils/count-options/index.js
+++ utils/count-options/index.js
@@ -117,7 +117,7 @@ const countOptions = async (database, query, fields, shouldCountMds, useSavedCou
     // If projects data is empty then stop here
     if (projectsData.length === 0) return {
         code: '404',
-        error: `The result of query "${query}" is empty`
+        error: `The result of query "${JSON.stringify(query)}" is empty`
     }
     // Set an object with project md counts
     const projectMdCounts = {};
